a9931c8fe49cb9e955f3dceaf58b0e5baee73adc
Dath <zenm9371@gmail.com>
2026-08-29 09:28:55 +0530

    Update logs.py

--- ROYEDITX/modules/logs.py
+++ ROYEDITX/modules/logs.py
@@ -1,50 +1,19 @@
 import asyncio
-import heroku3
 
-from config import X1, SUDO_USERS, OWNER_ID, HEROKU_API_KEY, HEROKU_APP_NAME, CMD_HNDLR as hl
+from config import X1, SUDO_USERS, CMD_HNDLR as hl
 from pyrogram import enums
 from datetime import datetime
 
 from telethon import events
-from telethon.errors import ForbiddenError
 
- 
+
 @X1.on(events.NewMessage(incoming=True, pattern=r"\%slogs(?: |$)(.*)" % hl))
 async def logs(legend):
     if legend.sender_id == enums.ChatMemberStatus.ADMINISTRATOR or enums.ChatMemberStatus.OWNER:
-        if (HEROKU_APP_NAME is None) or (HEROKU_API_KEY is None):
-            await legend.reply(
-                legend.chat_id,
-                "✦ First Set These Vars In Heroku ➥ `HEROKU_API_KEY` And `HEROKU_APP_NAME`.",
-            )
-            return
-
-        try:
-            Heroku = heroku3.from_key(HEROKU_API_KEY)
-            app = Heroku.app(HEROKU_APP_NAME)
-        except BaseException:
-            await legend.reply(
-                "✦ Make Sure Your Heroku API Key & App Name Are Configured Correctly In Heroku."
-            )
-            return
-
-        logs = app.get_log()
-        start = datetime.now()
-        fetch = await legend.reply(f"✦ Fetching Logs...")
-    
-        with open("AltLogs.txt", "w") as logfile:
-            logfile.write("✦ XSPAM ⚡ [ Bot Logs ]\n\n" + logs)
-
-        end = datetime.now()
-        ms = (end-start).seconds
-        await asyncio.sleep(1)
-
-        try:
-            await X1.send_file(legend.chat_id, "AltLogs.txt", caption=f"✦ **XSPAM BOT LOGS** ⚡\n\n● **ᴛɪᴍᴇ ᴛᴀᴋᴇɴ ➥** `{ms} ꜱᴇᴄᴏɴᴅꜱ`")
-            await fetch.delete()
-        except Exception as e:
-            await fetch.edit(f"✦ An Exception Occured, ERROR ➥ {str(e)}")
+        await legend.reply(
+            "✦ `ʟᴏɢꜱ ᴄᴏᴍᴍᴀɴᴅ ᴜɴᴀᴠᴀɪʟᴀʙʟᴇ`\n\n"
+            "➥ ᴛʜɪꜱ ʙᴏᴛ ɪꜱ ʜᴏꜱᴛᴇᴅ ᴏɴ ʀᴀɪʟᴡᴀʏ, ᴄʜᴇᴄᴋ ʟᴏɢꜱ ᴅɪʀᴇᴄᴛʟʏ ꜰʀᴏᴍ ᴛʜᴇ ʀᴀɪʟᴡᴀʏ ᴅᴀꜱʜʙᴏᴀʀᴅ."
+        )
 
     elif legend.sender_id in SUDO_USERS:
         await legend.reply("✦ ꜱᴏʀʀʏ, ᴏɴʟʏ ᴏᴡɴᴇʀ ᴄᴀɴ ᴀᴄᴄᴇꜱꜱ ᴛʜɪꜱ ᴄᴏᴍᴍᴀɴᴅ.")
-                         
